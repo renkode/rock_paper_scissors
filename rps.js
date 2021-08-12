049683b656d4ec5cfa671e1200813924edb89f8d
Renko <zaraku2@live.com>
2021-08-12 02:21:31 -0700

    it works!

--- rps.js
+++ rps.js
@@ -1,5 +1,9 @@
 const choices = ["rock","paper","scissors"];
 let compChoice;
+let choice;
+let playerScore = 0;
+let compScore = 0;
+let round = 0;
 
 function computerPlay() {
     // generate random index
@@ -14,64 +18,56 @@ const log = document.querySelector(".battle-log");
 log.textContent = "Select your weapon!";
 
 const weapons = document.querySelectorAll(".weapon");
+const playerPts = document.querySelector("#player-points");
+const compPts = document.querySelector("#comp-points");
 
-function selectWeapon (e) {
-    e.addEventListener('click',console.log(e.id))
-}
-
-weapons.forEach(selectWeapon);
-
-
+weapons.forEach(weapon => {
+    weapon.addEventListener('click', function (e) {
+        if (e.target.tagName === "BUTTON") choice = e.target.id;
+        if (e.target.tagName === "I") choice = e.target.parentNode.id;
+        game();
+    })
+});
 
 function playRound(playerSelection, computerSelection) {
     let victory;
     let draw;
     switch (true) {
-        case (playerSelection.toLowerCase() === computerSelection):
+        case (playerSelection === computerSelection):
             draw = true;
             break;r
-        case (playerSelection.toLowerCase() === "rock"):    
+        case (playerSelection === "rock"):    
             computerSelection === "scissors" ? victory = true : victory = false;
             break;
-        case (playerSelection.toLowerCase() === "paper"):    
+        case (playerSelection === "paper"):    
             computerSelection === "rock" ? victory = true : victory = false;
             break;
-        case (playerSelection.toLowerCase() === "scissors"):    
+        case (playerSelection === "scissors"):    
             computerSelection === "paper" ? victory = true : victory = false;
             break;
     }
     if (draw) return null;
     if (victory) return true;
     return false;
-  }
+}
 
-  function game(){
-    let playerScore = 0;
-    let compScore = 0;
+function game() {
     let roundResult;
-    for (let i=1; i<=5; i++) {
-      var playerChoice = prompt("Pick rock, paper, or scissors!");
-      // currently does not check if user enters a random string
-      computerPlay();
-      roundResult = playRound(playerChoice, compChoice); 
-      // determine round result
-      if (roundResult) {
-            console.log(`Round ${i}: You chose ${playerChoice.toLowerCase()}, computer chose ${compChoice}. You won this round!`);
-            playerScore++;
-      } else if (roundResult === false){
-            console.log(`Round ${i}: You chose ${playerChoice.toLowerCase()}, computer chose ${compChoice}. You lost this round!`);
-            compScore++;
-      } else {
-            console.log(`Round ${i}: You chose ${playerChoice.toLowerCase()}, computer chose ${compChoice}. It's a draw!`);
-      }
-      console.log(`Score: ${playerScore} pts (player) | ${compScore} pts (computer)`);
-    }
-    // determine game result
-    if (playerScore > compScore) {
-        return console.log("You won the game!");
-    } else if (playerScore < compScore) {
-        return console.log("Aww, you lost!");
-    } else {
-        return console.log("Neither wins!");
-    }
+    roundResult = playRound(choice, computerPlay());
+    if (roundResult) {
+        log.textContent = `Round ${round}: ${choice} > ${compChoice}.
+        You won this round!`;
+        playerScore++;
+  } else if (roundResult === false){
+        log.textContent = `Round ${round}: ${choice} < ${compChoice}.
+        You lost this round!`;
+        compScore++;
+  } else {
+        log.textContent = `Round ${round}: ${choice} = ${compChoice}.
+        It's a draw!`;
+  }
+  console.log(`Score: ${playerScore} pts (player) | ${compScore} pts (computer)`);
+  round++;
+  playerPts.textContent = `${playerScore} pts`;
+  compPts.textContent = `${compScore} pts`;
 }
