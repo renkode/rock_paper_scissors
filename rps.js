ecc665008467f27945052837927ce92ecef628c9
Renko <zaraku2@live.com>
2021-08-12 15:59:27 -0700

    polished layout

--- rps.js
+++ rps.js
@@ -3,30 +3,41 @@ let playerScore = 0;
 let compScore = 0;
 let round = 0;
 
-function computerPlay() {
-    // generate random index
-    let min = 0;
-    let max = choices.length;
-    let index = Math.floor(Math.random() * (max - min) + min);
-    return choices[index];
-}
-
 const log = document.querySelector(".battle-log");
 log.textContent = "Select your weapon!";
 
-const weapons = document.querySelectorAll(".weapon");
+const playerWeapons = document.querySelectorAll(".player-weapon");
+const compWeapons = document.querySelectorAll(".comp-weapon");
+const weapons = document.querySelectorAll(".player-weapon");
 const playerPts = document.querySelector("#player-points");
 const compPts = document.querySelector("#comp-points");
 
-weapons.forEach(weapon => {
+playerWeapons.forEach(weapon => {
     weapon.addEventListener('click', function (e) {
         let playerChoice;
-        let compChoice = computerPlay();
+        let choiceIndex = computerPlay();
+        let compChoice = choices[choiceIndex];
+        highlightCompChoice(choiceIndex);
         if (e.target.tagName === "BUTTON") playerChoice = e.target.id;
         if (e.target.tagName === "I") playerChoice = e.target.parentNode.id;
         playRound(outcome(playerChoice, compChoice), playerChoice, compChoice);
     })
 });
+
+function highlightCompChoice (choiceId) {
+    compWeapons.forEach(weapon => {
+        weapon.classList.remove("active");
+    });
+    compWeapons[choiceId].classList.add("active");
+}
+
+function computerPlay() {
+    // generate random index
+    let min = 0;
+    let max = choices.length;
+    let index = Math.floor(Math.random() * (max - min) + min);
+    return index;
+}
 
 function outcome(playerSelection, computerSelection) {
     let victory;
@@ -52,15 +63,15 @@ function outcome(playerSelection, computerSelection) {
 
 function playRound(result, playerChoice, compChoice) {
     if (result) {
-        log.textContent = `Round ${round}: ${playerChoice} > ${compChoice}.
+        log.textContent = `Round ${round}: ${playerChoice} vs. ${compChoice}.
         You won this round!`;
         playerScore++;
   } else if (result === false){
-        log.textContent = `Round ${round}: ${playerChoice} < ${compChoice}.
+        log.textContent = `Round ${round}: ${playerChoice} vs. ${compChoice}.
         You lost this round!`;
         compScore++;
   } else {
-        log.textContent = `Round ${round}: ${playerChoice} = ${compChoice}.
+        log.textContent = `Round ${round}: ${playerChoice} vs. ${compChoice}.
         It's a draw!`;
   }
   console.log(`Score: ${playerScore} pts (player) | ${compScore} pts (computer)`);
